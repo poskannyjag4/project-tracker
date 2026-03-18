cd54df63140a91599efb743f90098e2f7f018151
poskannyjag4 <poskannyjag@gmail.com>
2026-03-18 15:31:51 +0700

    fix(vite.config): change base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/my-pwa-app/', //поменяй
+  base: '/project-tracker/', //поменяй
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
