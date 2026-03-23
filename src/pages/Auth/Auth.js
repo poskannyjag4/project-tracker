196947bc0bdffbf30d4a19ab3839836875049ebc
poskannyjag4 <poskannyjag@gmail.com>
2026-03-23 20:28:42 +0700

    fix(auth screen): change google authentication, add adaptive styles

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -3,11 +3,11 @@ import { loginUser } from '../../api/auth';
 
 export function Auth(){
     //base container
-    const screen = document.createElement('div');
+    const screen = document.createElement('main');
     screen.classList.add(styles.container);
 
     //left side container
-    const leftSide = document.createElement('div');
+    const leftSide = document.createElement('section');
     leftSide.classList.add(styles.left);
     
     // Project Title
@@ -23,47 +23,35 @@ export function Auth(){
     span.textContent = "chaos";
     slogan.append(span);
 
-    //Google Setup
-    const googleSetup = document.createElement('div');
-    googleSetup.id = "g_id_onload";
 
     //add function to global window scope
     window.loginUser = loginUser;
 
     //google settings for GIS
-    googleSetup.setAttribute('data-client_id', import.meta.env.VITE_GOOGLE_CLIENT_ID);
-    googleSetup.dataset.callback = loginUser.name;
-    googleSetup.dataset.auto_prompt = "true";
-    googleSetup.dataset.itp_support = "true";
+    const configDiv = document.createElement('div');
+    configDiv.id = "g_id_onload";
+    configDiv.setAttribute('data-client_id', import.meta.env.VITE_GOOGLE_CLIENT_ID);
+    configDiv.setAttribute('data-callback', 'loginUser');
+    configDiv.setAttribute('data-context', 'signin');
+    configDiv.setAttribute('data-ux_mode', 'popup');
+    configDiv.setAttribute('data-auto_prompt', 'true');
+    configDiv.setAttribute('data-itp_support', 'true');
     
     //Google login btn
-    const googleBtn = document.createElement('div');
-    googleBtn.classList.add(styles.googleBtn);
 
-    //Google logo
-    googleBtn.innerHTML = `
-                <svg viewBox="0 0 24 24">
-                    <path  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                    <path  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                    <path  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z"/>
-                    <path  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 12-5.38z"/>
-                </svg>
-    `;
-    googleBtn.append('Continue with Google');
+  const buttonDiv = document.createElement('div');
+    buttonDiv.className = "g_id_signin";
+    buttonDiv.setAttribute('data-type', 'standard');
+    buttonDiv.setAttribute('data-shape', 'rectangular');
+    buttonDiv.setAttribute('data-theme', 'outline');
+    buttonDiv.setAttribute('data-text', 'signin_with');
+    buttonDiv.setAttribute('data-size', 'large');
+    buttonDiv.setAttribute('data-logo_alignment', 'left');
 
-    //sugnin Popup on click
-    googleBtn.onclick = () => {
-    google.accounts.id.prompt((notification) => {
-        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-            google.accounts.id.prompt();
-        }
-    });
-  };
-
-    leftSide.append(title, slogan, googleBtn, googleSetup);
+    leftSide.append(title, slogan,configDiv, buttonDiv);
 
     // right side container
-    const rightSide = document.createElement('div');
+    const rightSide = document.createElement('aside');
     rightSide.classList.add(styles.right);
 
     //bento grid
